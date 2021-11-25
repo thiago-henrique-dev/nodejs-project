776f590359cff32cfd6782f4c71589b889ae72f5
thiago-henrique-dev <thiagohenriquedev@gmaail.com>
2021-11-25 10:09:30 -0300

    up

--- index.js/index.js
+++ index.js/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World, install nodejs");
+const http =  require('http');
+
+let server = http.createServer((req, res)=>{
+
+    console.log('URL:', req.url);
+    console.log('METHOD:', req.method);
+
+    res.end("OK");
+
+})
+
+server.listen(3000, '127.0.0.1', ()=>{
+
+    console.log('servidor rodando!')
+
+})
