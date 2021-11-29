8d250f47905608c114f8352c071a9b46a73ba870
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2021-11-28 22:32:18 -0200

    Consumindo Rotas, POST, PUT e DELETE com Restify

--- client-server/public/controllers/UserController.js
+++ client-server/public/controllers/UserController.js
@@ -199,24 +199,9 @@ class UserController {
 
     selectAll(){
 
-       //let users = User.getUsersStorage();
-       let ajax = new XMLHttpRequest();
-
-       ajax.open('GET', '/users');
-
-
-       ajax.onload = event => {
-
-            let obj = { users: []}
-
-            try{
-                let obj = JSON.parse(ajax.responseText);
-            } catch(e){
-
-                console.error(e)
-            }
-           
-           obj.users.forEach(dataUser=>{
+       HttpRequest.get('/users').then(data=>{
+        
+        data.users.forEach(dataUser=>{
 
             let user = new User();
 
@@ -225,11 +210,8 @@ class UserController {
             this.addLine(user);
 
         });
-        
-       }
-    
-       ajax.send()
 
+       });
 
     }
 
